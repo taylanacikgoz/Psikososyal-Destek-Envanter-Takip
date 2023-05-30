7816015f0101f4e413df57624028676f40639972
elifgamzededeler <gamze.dedeler@gmail.com>
2023-05-30 16:39:30 +0300

    tablolar eklendi, navigation güncellendi

--- server/api/Routes/SehirRoutes/getData-model.js
+++ server/api/Routes/SehirRoutes/getData-model.js
@@ -1,12 +1,15 @@
 const db = require("../../../data/dbConfig");
 
 const getAll = async () => {
-  const Sehirler = await db("Sehir as s")
-    .leftJoin("Merkez as m", "s.Sehir_id", "m.Sehir_id")
-    .leftJoin("Personel as p", "p.Merkez_id", "m.Merkez_id")
-    .leftJoin("Envanter as e", "e.Merkez_id", "m.Merkez_id")
-    .leftJoin("Kurum as k", "k.Merkez_id", "m.Merkez_id")
-    .leftJoin("Hizmet as h", "h.Merkez_id", "m.Merkez_id");
+  const Sehirler = await db("Sehir as s").leftJoin(
+    "Merkez as m",
+    "s.Sehir_id",
+    "m.Sehir_id"
+  );
+  // .leftJoin("Personel as p", "p.Merkez_id", "m.Merkez_id")
+  // .leftJoin("Envanter as e", "e.Merkez_id", "m.Merkez_id")
+  // .leftJoin("Kurum as k", "k.Merkez_id", "m.Merkez_id")
+  // .leftJoin("Hizmet as h", "h.Merkez_id", "m.Merkez_id");
   return Sehirler;
 };
 
